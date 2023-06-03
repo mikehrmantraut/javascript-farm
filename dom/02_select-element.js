f914f05dbc3764a96554de46b582a09125d4d680
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2023-06-03 18:24:29 +0300

    dom folder added.

---
+++ dom/02_select-element.js
@@ -0,0 +1,6 @@
+let title = document.getElementsById('title')
+title.innerHTML = "Change"
+console.log(title.innerHTML)
+
+let link = document.querySelector("ul#list>li>a")
+console.log(link.innerHTML)
